98b0bbe8db45f052282564b3bac3342bc307f8e2
Jude Hosmer <jude@crc-dot1x-nat-10-239-35-96.bu.edu>
2025-10-10 01:23:26 -0400

    Implemented fetch function to fetch data from API and display movie information and images

--- services/api.ts
+++ services/api.ts
@@ -3,14 +3,14 @@ export const TMDB_CONFIG = {
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}'
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
 }
 
 export const fetchMovies = async({ query }:{ query: string}) => {
     const endpoint = query
-    ? '${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIcomponent(query)}'
-    : '${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc';
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: 'GET',
@@ -18,7 +18,7 @@ export const fetchMovies = async({ query }:{ query: string}) => {
     });
     if(!response.ok) {
         // @ts-ignore
-        throw new Error('Failed to fetch movies', response.statusText);
+        throw new Error(`Failed to fetch movies`, response.statusText);
     }
 
     const data = await response.json();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      colors: {
+      colors: { 
         primary: '#030014',
         secondary: '#151312',
         accent: '#AB8BFF',
